d588072df8460d847db30d202f42e0df67b3e3bc
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-07-19 12:54:22 +0430

    Add wrok section with slide show

--- code/index.js
+++ code/index.js
@@ -4,4 +4,27 @@ window.addEventListener("DOMContentLoaded", function (e) {
     menuIcon.classList.toggle("open");
     document.querySelector("nav").classList.toggle("active");
   });
+
+  const wrapper = document.querySelector(".carousel-wrapper");
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.querySelectorAll(".dot");
+  slides[1].scrollIntoView({
+    behavior: "smooth",
+    inline: "center",
+    block: "nearest",
+  });
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", (e) => {
+      dots.forEach((dot) => dot.classList.remove("active"));
+
+      slides[index + 1].scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest",
+      });
+
+      dot.classList.add("active");
+    });
+  });
 });
